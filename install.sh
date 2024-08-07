0acb632219f97e2d7347532c06d12f426960e9ed
dqsully <djqballer@outlook.com>
2024-08-06 20:51:42 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 
 mkdir -p ~/.sh-pkg/sources ~/.sh-pkg/remote-sources
 git clone https://github.com/dqsully/sh-pkg ~/.sh-pkg/remote-sources/std
-ln -s ~/.sh-pkg/remote-sources/std ~/.sh-pkg/sources/std
+ln -s ~/.sh-pkg/remote-sources/std/packages ~/.sh-pkg/sources/std
 ~/.sh-pkg/sources/std/packages/sh-pkg/bin/sh-pkg install std/sh-pkg
