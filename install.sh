de0c745d01288212f406766841b002652c723dc9
Dakota Sullivan <djqballer@outlook.com>
2024-09-11 10:15:59 -0600

    First-install bugfixes

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir -p ~/.sh-pkg/sources ~/.sh-pkg/remote-sources
-git clone https://github.com/dqsully/sh-pkg ~/.sh-pkg/remote-sources/std
-ln -s ~/.sh-pkg/remote-sources/std/packages ~/.sh-pkg/sources/std
+mkdir -p ~/.sh-pkg/sources ~/.sh-pkg/remote_sources
+git clone https://github.com/dqsully/sh-pkg ~/.sh-pkg/remote_sources/std
+ln -s ~/.sh-pkg/remote_sources/std/packages ~/.sh-pkg/sources/std
 ~/.sh-pkg/sources/std/sh-pkg/bin/sh-pkg install sh-pkg
